aad172d43ffb7172d19df6f7ff5617d094627aaa
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-06-11 02:55:49 -0600

    ++Upgraded to 2019.4 ++Added the new Unity Input Package (should probably remove it because I don't want to use anything unfamiliar with this new project) ++Started working on logic to move panels up the screen.

---
+++ PanelProject/Assets/Scripts/BoardController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BoardController : MonoBehaviour
+{
+    public static BoardController instance;
+    public List<Sprite> characters;
+    [SerializeField] GameObject panel;
+    public int xSize, ySize;
+    public int startingHeight;
+
+    private GameObject[,] panels;
+    Vector2 offset;
+
+    public bool IsShifting { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = GetComponent<BoardController>();
+
+        offset = panel.GetComponent<SpriteRenderer>().bounds.size;
+        CreateBoard(offset.x, offset.y);
+    }
+
+    private void CreateBoard(float xOffset, float yOffset)
+    {
+        panels = new GameObject[xSize, ySize];
+
+        float startX = transform.position.x;
+        float startY = transform.position.y;
+
+        for(int x = 0; x < xSize; x++)
+        {
+            for(int y = 0; y < ySize; y++)
+            {
+                if (y < startingHeight)
+                {
+                    GameObject newPanel = Instantiate(panel,
+                                new Vector3(startX + (xOffset * x), startY + (yOffset * y)),
+                                panel.transform.rotation,
+                                this.transform);
+                    panels[x, y] = newPanel; 
+                }
+                else
+                {
+                    panels[x, y] = null;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            CreateNewRow();
+        }
+    }
+
+    public void CreateNewRow()
+    {
+        if (MoveUp())
+        {
+        }
+    }
+
+    /// <summary>
+    /// Atempts to move all of the panels up one row. If it cant, it sets the lose condition and returns false.
+    /// </summary>
+    /// <returns></returns>    
+    public bool MoveUp()
+    {
+        Debug.Log("Moving panels up");
+        for (int x = xSize - 1; x >= 0; x--)
+        {
+            for (int y = ySize - 1; y >= 0 ; y--)
+            {
+                if(panels[x,y] == null) { continue; }
+                if(y >= ySize) { return false; }//TODO need to test this
+                Vector3 originalPosition = panels[x, y].transform.position;
+                panels[x, y].transform.position = new Vector3(originalPosition.x, originalPosition.y + offset.y);
+                panels[x, y + 1] = panels[x, y];//Move it up one block
+            }
+        }
+        return true;
+    }
+}
---
+++ PanelProject/Assets/Scripts/Panel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Panel clicked on");
+    }
+}
