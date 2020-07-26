040812d07c22282b79558e2b69dd541964c87189
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-07-26 02:54:08 -0600

    Added the music I liked best.
    ++Added the main song. I'd like to look at using an audio mixer to change tracks.
    ++Fixed some scaling problems with the UI.
    
    TODO:
    Figure out and try to fix that bug.
    There is too much of a delay between a match being made and being cleared...

--- PanelProject/Assets/Scripts/PauseMenu.cs
+++ PanelProject/Assets/Scripts/PauseMenu.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab8ffe89eea354d00068133524a4e20e836fadd25a22e0e7822cf7adddfef785
-size 538
+oid sha256:3630e449a8dbe7c95d680ae89651227b771c94981b0b9248b821a95c9eb737f1
+size 604
